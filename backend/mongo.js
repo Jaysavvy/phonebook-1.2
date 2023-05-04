b2f094caa27bff29df476c0ea83aa8a1c0c62c77
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-05-04 13:41:25 -0400

    Updated mongodb file w/ process.argv

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,33 +1,52 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// if (process.argv.length<3) {
-//   console.log('give password as argument')
-//   process.exit(1)
-// }
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
 
-// const password = process.argv[2]
+if (process.argv.length > 5) {
+  console.log(
+    "Please provide maximum of 3 arguments: node mongo.js <password> <name> <number>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 
 const url = process.env.Mongo_DB;
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.connect(url),
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  };
 
 const phoneSchema = new mongoose.Schema({
-  content: String,
-  phone: String,
+  name: String,
+  number: String,
   important: Boolean,
 });
 
 const PhoneNumber = mongoose.model("PhoneNumber", phoneSchema);
 
 const phone = new PhoneNumber({
-  content: "Pappa John",
-  phone: "541-234-0875",
-  important: true,
+  name,
+  number,
 });
 
-phone.save().then((result) => {
-  console.log("number saved!");
-  mongoose.connection.close();
-});
+process.argv.length > 3
+  ? phone.save().then(() => {
+      console.log(`added ${name} number ${number} to phonebook`);
+      mongoose.connection.close();
+    })
+  : PhoneNumber.find({}).then((people) => {
+      console.log(`${phone.name} ${phone.number}`);
+    });
